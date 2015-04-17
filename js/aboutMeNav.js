84be5fd037fd9a109c2759f245608344435bbf00
Sung Kim <sunggyun187@gmail.com>
2015-04-17 16:00:19 -0400

    added transitions to the about me info tabs

--- js/aboutMeNav.js
+++ js/aboutMeNav.js
@@ -2,8 +2,20 @@
 
 $('.nav_btn').each(function(index) {
   $(this).on('click', function(){
-    $('.active').removeClass('active')
+    $('.nav_btn.selected').removeClass('selected')
+    $(this).addClass('selected');
+    $('.active').removeClass('active');
     var info = $('.info').eq(index);
     info.toggleClass('active');
+  });
+});
+
+$('.clin_btn').each(function(index) {
+  $(this).on('click', function(){
+    $('.clin_btn.selected').removeClass('selected')
+    $(this).addClass('selected');
+    $('.clin.info.active').removeClass('active');
+    var clin_info = $('.clin.info').eq(index);
+    clin_info.toggleClass('active');
   });
 });
