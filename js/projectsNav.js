e1597b6743cd2942fb316b223642f3bd316f51f6
Sung Kim <sunggyun187@gmail.com>
2015-05-11 20:16:53 -0400

    add better transitions for tabs and did more responsive design

--- js/projectsNav.js
+++
@@ -1,9 +0,0 @@
-$('.project_btn').each(function(index) {
-  $(this).on('click', function(){
-    $('.project_btn.selected').removeClass('selected')
-    $(this).addClass('selected');
-    $('.project.info.active').removeClass('active');
-    var info = $('.project.info').eq(index);
-    info.toggleClass('active');
-  });
-});
